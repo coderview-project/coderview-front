e1abad38ad80056ce56b3ec5e9a613a9a19e4f4a
Ana Garrido <anagarridocoder@gmail.com>
2023-04-13 13:21:29 +0200

    admin-view process

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HomeView from './pages/HomeView'
+import AdminView from './pages/AdminView'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HomeView />
+    <AdminView />
     
   </React.StrictMode>,
 )
