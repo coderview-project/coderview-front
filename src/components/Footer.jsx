b7d07c6d8f59a22bc895aaba30c467b8ce2d740f
Ana Garrido <anagarridocoder@gmail.com>
2023-04-18 11:35:08 +0200

    Finished Admin-view without responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 const Footer = () => {
     return (
 
-        <div className="h-12 text-white text-center font-poppins bg-orange">
+        <div className="h-12 text-white text-center font-poppins  bg-orange">
             <h1 className= "text-sm">Factoría F5 @ 2023
             <br></br>
             Todos los derechos reservados</h1>
--- src/pages/AdminView.jsx
+++ src/pages/AdminView.jsx
@@ -5,33 +5,49 @@ function AdminView() {
 
     return (
 
-        <div>
-            <div className="bg-cover bg-top h-20 w-full bg-orange object-fit"> <p className="text-white text-left text-3xl font-poppins px-4 py-5"> Lidia García </p>
-                <div className="flex px-96 mx-24">
+        <div className= "">
 
-                    <button type="button" className="text-center"><img className="h-16 w-16" src="src\assets\icon _plus circle_.png" alt="create-bootcamp"></img> Crear Bootcamp </button>
+            <div className="bg-cover bg-top h-24 bg-orange object-fit"> <p className="text-white text-left text-3xl font-poppins px-4 py-7"> Lidia García </p>
+            </div>
 
-                    <button type="button" className=""><img className="h-16 w-16" src="src\assets\icon _gear_.png" alt="edit-bootcamp"></img> Editar Bootcamp</button>
+            <div className="flex flex-center float-right text-xl font-poppins text-center">
 
-                    <button type="button" className=""><img className="h-16 w-16" src="src\assets\icon _minus circle_.png" alt="edit-bootcamp"></img> Eliminar Bootcamp</button>
+                <button type="button" className="flex flex-center mx-8 my-12"><img className="h-16 w-16" src="src\assets\icon _plus circle_.png" alt="create-bootcamp"></img><p className="py-4 px-2">Crear Bootcamp</p></button>
+
+                <button type="button" className="flex flex-center mx-8 my-12"><img className="h-16 w-16" src="src\assets\icon _gear_.png" alt="edit-bootcamp"></img><p className="py-4 px-2">Editar Bootcamp</p></button>
+
+                <button type="button" className="flex flex-center mx-8 my-12"><img className="h-16 w-16" src="src\assets\icon _minus circle_.png" alt="edit-bootcamp"></img><p className="py-4 px-2">Eliminar Bootcamp</p></button>
+
+            </div>
+
+            <div className="my-9 grid grid-cols-2">
+
+                <div className="py-12 grid grid-col text-center ">
+                    <img className="flex-auto w-96 h-96 relative my-10 inset-x-8 " src="src\assets\admin-photo.png" alt="admin-photo"></img>
+
+                    <button type="button" className="flex items-center bg-orange w-56 h-22 my-10 mx-28 p-2 rounded-3xl"><img className="px-2 py-1" src="src\assets\icon _pencil_.png" alt="edit-profile" />Editar perfil personal</button>
+
+                </div>
+
+                <div className="mx-96 my-28">
+                    <button type="button" className="flex items-center uppercase bg-salmon w-64 h-22 my-10 mx-28 p-2 rounded-3xl"><img className="px-2 py-1 uppercase" src="src\assets\icon _pencil_.png" alt="edit-profile" />Femcoders Sevilla</button>
+
+                    <button type="button" className="flex items-center uppercase bg-salmon w-64 h-22 my-10 mx-28 p-2 rounded-3xl"><img className="px-2 py-1 uppercase" src="src\assets\icon _pencil_.png" alt="edit-profile" />Femcoders Barcelona</button>
+
+                    <button type="button" className="flex items-center uppercase bg-salmon w-64 h-22 my-10 mx-28 p-2 rounded-3xl"><img className="px-2 py-1 uppercase" src="src\assets\icon _pencil_.png" alt="edit-profile" />Front-end Asturias</button>
+
                 </div>
 
             </div>
 
-
-            <div className=" w-72 h-72 flex-auto text-center">
-                <img className="flex-auto relative my-10 inset-x-8 " src="src\assets\admin-photo.png" alt="admin-photo"></img>
-
-                <button type="button" className="flex items-center bg-orange w-56 h-22 my-10 mx-28 p-2 rounded-3xl"><img className="px-2 py-1" src="src\assets\icon _pencil_.png" alt="edit-profile" />Editar perfil personal</button>
-
+            <div className="h-1.5">
+                
             </div>
 
             <Footer />
-        </div>
+        </div >
+        
 
     )
 }
 export default AdminView
-
-//<div class="flex-initial text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-//Medium length
