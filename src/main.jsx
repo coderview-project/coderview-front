c1153e85fc5fda1c7f3d193dcd8bd77bed6995c8
Ana Garrido <anagarridocoder@gmail.com>
2023-04-19 14:32:13 +0200

    created AdminBootcampView page

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AdminView from './pages/AdminView'
+import AdminBootcampView from './pages/AdminBootcampView'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AdminView />
+    <AdminBootcampView />
     
   </React.StrictMode>,
 )
---
+++ src/pages/AdminBootcampView.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../components/Footer'
+
+function AdminBootcampView() {
+
+    return (
+        <div>
+            <Footer />
+        </div>
+    )
+}
+
+export default AdminBootcampView
