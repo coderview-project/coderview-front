b3bc733521a378cf7758f5eb39ef72326eb43575
Delia MunMol <delia.m.m@outlook.es>
2023-04-20 13:45:50 +0200

    IMG respnosive done

--- src/pages/AdminView.jsx
+++ src/pages/AdminView.jsx
@@ -12,11 +12,11 @@ function AdminView() {
             </div>
 
             <div className="flex flex-col">
-                <div className="Buttons-bootcamp flex justify-center float-right mt-6 text-xl font-poppins text-center">
+                <div className="Buttons-bootcamp flex justify-center float-right md:mt-6 ml-10 mt-6 text-xl font-poppins text-center">
 
-                    <button type="button" className="Create flex flex-center"><img className="h-12 w-12 mt-1" src="src\assets\icon _plus circle_.png" alt="create-bootcamp"></img><p className="py-4 px-2">Crear Bootcamp</p></button>
+                    <button type="button" className="Create flex flex-center"><img className="h-12 w-12 mt-1 md: mb-4" src="src\assets\icon _plus circle_.png" alt="create-bootcamp"></img><p className="py-4 px-2">Crear Bootcamp</p></button>
 
-                    <button type="button" className="Edit flex flex-center mx-10"><img className="h-12 w-12 mt-1" src="src\assets\icon _gear_.png" alt="edit-bootcamp"></img><p className="py-4 px-2">Editar Bootcamp</p></button>
+                    <button type="button" className="Edit flex flex-center md:mx-10"><img className="h-12 w-12 mt-1 md: mb-4" src="src\assets\icon _gear_.png" alt="edit-bootcamp"></img><p className="py-4 px-2">Editar Bootcamp</p></button>
 
                     <button type="button" className="flex flex-center"><img className="h-12 w-12 mt-1" src="src\assets\icon _minus circle_.png" alt="edit-bootcamp"></img><p className="py-4 px-2">Eliminar Bootcamp</p></button>
 
@@ -25,11 +25,11 @@ function AdminView() {
                 <div className="Photo flex justify-center flex-cols-2 font-poppins">
 
                     <div className="flex flex-col text-center">
-                        <div className="w-80 h-80 mb-8 ml-8">
+                        <div className="w-40 h-40 mb-8 ml-8 md:w-80 md:h-80 ">
                             <img className="object-cover relative my-12 inset-x-8 " src="src\assets\admin-photo.png" alt="admin-photo"></img>
                         </div>
 
-                        <button type="button" className="flex items-center bg-orange w-56 h-22 my-10 mx-28 p-2 rounded-3xl"><img className="px-2 py-1" src="src\assets\icon _pencil_.png" alt="edit-profile" />Editar perfil personal</button>
+                        <button type="button" className="flex items-center bg-orange w-56 h-22 my-10 mx-28 p-2 rounded-3xl md: m-2"><img className="px-2 py-1" src="src\assets\icon _pencil_.png" alt="edit-profile" />Editar perfil personal</button>
 
                     </div>
 
