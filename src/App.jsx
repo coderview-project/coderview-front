7b10d56960c67063becffbecf8b695e9dcab19b8
Ana Garrido <anagarridocoder@gmail.com>
2023-04-24 15:23:49 +0200

    AdminBootcampView almost finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import AdminView from './pages/AdminView'
+import AdminBootcampView from './pages/AdminBootcampView'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,7 @@ function App() {
     <div>
       
       {/*<AdminBootcampView />*/}
-      <AdminView />
+      <AdminBootcampView />
 
     </div>
   )
--- src/pages/AdminBootcampView.jsx
+++ src/pages/AdminBootcampView.jsx
@@ -5,14 +5,52 @@ import Footer from '../components/Footer'
 function AdminBootcampView() {
 
     return (
-        <div className=" py-48">
+        <div>
             <Navb />
-            <div className="flex flex-auto !important">
+            <div className="flex">
 
-                <div className="Name flex flex-row-reverse bg-white text-orange text-3xl font-poppins"><p className="py-4 px-1">Lidia García</p><img src="src\assets\admin-photo.png" className="h-20 w-20"></img>
+                <div className="flex">
+
+                    <div className="Name mx-4 md:mx-0 flex h-16 bg-white text-orange text-2xl font-poppins md:text-3xl md:pl-28"><p className="px-6 pl-6 md:pl-0 text-center">Lidia García</p>
+                    </div>
+
+                    <div className="Bar mx-auto h-16 md:w-screen bg-salmon font-poppins text-center text-white text-xl md:text-3xl"><p className='md:py-4 md:pr-44'>FEMCODERS SEVILLA</p></div>
                 </div>
 
-                <div className= "Bar h-18 px-72 bg-salmon font-poppins text-center text-white text-xl">FEMCODERS SEVILLA</div>
+
+
+            </div>
+            <div className='flex flex-col md:flex-row md:py-12'>
+
+                <div className='flex flex-col py-6 '>
+
+                    <div className="flex font-poppins text-white"> <button className=" bg-orange w-56 h-16 my-10 mx-20 p-2 rounded-3xl">GESTIONAR COMPETENCIAS</button>
+                    </div>
+                    <div className="flex font-poppins text-white"> <button className="bg-orange w-56 h-16 my-10 mx-20 p-2 rounded-3xl">GESTIONAR ALUMN@S</button>
+                    </div>
+                    <div className="flex font-poppins text-white"> <button className="bg-orange w-56 h-16 my-10 mx-20 p-2 rounded-3xl">GESTIONAR EVALUACIONES</button>
+                    </div>
+                </div>
+
+
+                <div className="xcoder-box flex font-poppins md:h-auto md:w-screen">
+                    <div className='flex md:flex-col w-1/2 font-poppins text-center text-2xl'>Alumn@s
+                        <div>
+
+
+                        </div>
+                    </div>
+
+                    <div className='flex flex-row md:flex-col w-1/2 font-poppins text-center text-2xl'>Email
+                        <div>
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+
             </div>
             <Footer />
         </div>
