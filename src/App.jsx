1a51acff07a9f7391e58ce12f6899f0de0cc8393
Margarita Álvarez González <karessibienestar@gmail.com>
2023-04-28 13:12:43 +0200

    return the previous version slider

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ function App() {
     
     <>     
       {/* <Navb/> */}
-      <Form />
+      
 
-      {/* <Range /> */}
+      <Range />
       {/* <HomeView/> */}
       {/* <Footer/> */}
     </>  
--- src/components/Range.jsx
+++ src/components/Range.jsx
@@ -1,47 +1,49 @@
 import '../Style/Range.css'
 import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
 import WateringCan from "../assets/WateringCan.png"
 import Garden from "../assets/Garden.png"
 
-function Range({formData, setFormData, question}){
+function Range(){
 
-    const [sliderValue, setSliderValue] = useState(`${formData[question]}`);
+    const [sliderValue, setSliderValue] = useState(3);
+ 
 
-              
     function customRangerSlider(event) {
         let sliderElement = event.target.parentElement;
         let slider = sliderElement.querySelector(".slider");
         let sliderThumb = sliderElement.querySelector('.slider-thumb');
-        
         let progressElement = sliderElement.querySelector(".progress");
         const value = (slider.value/slider.max) * 100;
+        console.log(value);
         progressElement.style.width = value;
         sliderThumb.style.cssText = `left: ${value}%; transform:translate(-${value}%, -50%);`
-        setSliderValue(Math.round((value / 100) * 6));
-        setFormData({...formData, projectQ : (Math.round((value / 100) * 6))}); 
-        console.log(sliderValue)            
+        setSliderValue(Math.round((value / 100) * 6));             
     }
 
+
     return(
-    <div className="body"> 
-        <div className="range-slider">          
-            <input onChange={customRangerSlider} type="range" min={0} max={6}  value={sliderValue} className= "slider"/> 
-            <img className="rangeimg" src={Garden} alt=""/>             
-                <div className="slider-thumb">                      
+        <div className="body"> 
+        
+        <div className="range-slider">  
+            <input onChange={customRangerSlider} type="range" min={0} max={6}  value={sliderValue} 
+            className= "slider"/>   
+                       
+                <div className="slider-thumb">   
+                                   
                     <div className="tooltip">
                         <div className="tooltip-img">
                             <img 
                             src={WateringCan} alt="" />
                         </div>
                         <p>{sliderValue}</p>
-                        
-                    </div>                    
+                    </div>
                 </div>  
-                    <div className="progress" >
-                   </div>                    
+                   <img className="rangeimg" src={Garden} alt=""/>
+                    <div className="progress">
+                        
+                    </div>
         </div>
-    </div> 
+    </div>   
     );  
 }
 
