eeb98b40921de2f87c6d96ab22b9474d0d220de6
PaolaArcePavez <politaarce@gmail.com>
2023-04-28 10:42:01 +0200

    change word

--- src/components/FunctionalTechnical.jsx
+++ src/components/FunctionalTechnical.jsx
@@ -1,6 +1,6 @@
 
 function FunctionalTechnical({formData, setFormData}) {
-    return(
+    return (
         <>
         <div className="flex flex-col justify-center items-center">
         <label className=""> ¿Cómo de capaz eres de conceptualizar de manera funcional un proyecto digital? </label>
@@ -9,7 +9,7 @@ function FunctionalTechnical({formData, setFormData}) {
         <label> ¿Cómo de capaz eres de diseñar los procesos lógicos necesarios para un proyecto digital? </label>
         <input type="range" id="evaluation" name="evaluation"
          min="0" max="6" value={formData.funcTech2} onChange={(event) => setFormData({...formData, funcTech2: event.target.value})}/>
-        <label> ¿Y de explicar la solución adoptada y presentar el trabajo realziado sintetizando sus planteamientos y resultados? </label>
+        <label> ¿Y de explicar la solución adoptada y presentar el trabajo realizado sintetizando sus planteamientos y resultados? </label>
         <input type="range" id="evaluation" name="evaluation"
          min="0" max="6" value={formData.funcTech3} onChange={(event) => setFormData({...formData, funcTech3: event.target.value})}/>
         </div>
