2576789e8b28c7016ebfe948a7c7601260d916b2
Margarita Álvarez González <karessibienestar@gmail.com>
2023-05-03 12:40:28 +0200

    added style myEvaluation

--- src/components/QA.jsx
+++ src/components/QA.jsx
@@ -6,15 +6,15 @@ function QA({formData, setFormData}) {
     return(
         <>
         <div className="flex flex-col justify-center items-center mt-4">
-         <label  > ¿Qué capaz eres de implementar prácticas y tecnologías para la gestión de versiones? </label>
+         <label className="flex flex-col justify-center items-center mt-4"  > ¿Qué capaz eres de implementar prácticas y tecnologías para la gestión de versiones? </label>
          <img src={RangeSlider1}/>
         <input className="RangeSlider" type="range" id="evaluation" name="evaluation"
          min="0" max="6" value={formData.qa1} onChange={(event) => setFormData({...formData, qa1: event.target.value})}/>
-         <label > ¿Cómo de capaz eres de implementar un código limpio y escalable? </label>
+         <label className="flex flex-col justify-center items-center mt-4" > ¿Cómo de capaz eres de implementar un código limpio y escalable? </label>
          <img src={RangeSlider1}/>
         <input className="RangeSlider" type="range" id="evaluation" name="evaluation"
          min="0" max="6" value={formData.qa2} onChange={(event) => setFormData({...formData, qa2: event.target.value})}/>
-        <label> ¿Y de implementar prácticas de QA para el aseguramiento y control de la calidad de una web? </label>
+        <label className="flex flex-col justify-center items-center mt-4" > ¿Y de implementar prácticas de QA para el aseguramiento y control de la calidad de una web? </label>
         <img src={RangeSlider1}/>
         <input className="RangeSlider" type="range" id="evaluation" name="evaluation"
          min="0" max="6" value={formData.qa3} onChange={(event) => setFormData({...formData, qa3: event.target.value})}/>
--- src/pages/MyEvaluations.jsx
+++ src/pages/MyEvaluations.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { evaluationHandler } from "../handler/evaluationHandler";
+import WateringCan from "../assets/WateringCan.png"
+import  '../Style/RangeSlider.css'
 
 
 function MyEvaluations() {
@@ -25,26 +27,56 @@ function MyEvaluations() {
     } else {
         return (
             <>
-            <h1> MIS AUTOEVALUACIONES </h1> 
-                {evaluationData.map((evaluation, index) => {
-                    return(
-                        <>
-                        <h1>  AUTOEVALUACIÓN {index + 1} </h1>
-                   
-                        <h1> {evaluation.projectM}</h1>
-                        <h1> {evaluation.funcTech1}</h1>
-                        <h1> {evaluation.funcTech2}</h1> 
-                        <h1> {evaluation.funcTech3}</h1> 
-                        <h1> {evaluation.front1}</h1> 
-                        <h1> {evaluation.front2}</h1> 
-                        <h1> {evaluation.back1}</h1> 
-                        <h1> {evaluation.back2}</h1> 
-                        <h1> {evaluation.archit}</h1> 
-                        <h1> {evaluation.qa1}</h1> 
-                        <h1> {evaluation.qa2}</h1> 
-                        <h1> {evaluation.qa3}</h1> 
-                        </>
-                    )})}
+            <LoggedNavb/>
+            <div className="flex flex-col font-poppins">
+                <h1 className="text-center text-3xl"> MIS AUTOEVALUACIONES </h1> 
+                    {evaluationData.map((evaluation, index) => {
+                        return(
+                            <>
+                            <h1>  AUTOEVALUACIÓN {index + 1} </h1>
+                            <div ClassName="flex flex-col justify-items-start mt-4">
+
+                                <p>¿Cómo de capaz eres de planificar y gestionar un proyecto digital a través de marcos de trabajo?</p>                  
+                                <img src={WateringCan} alt="Evaluation result icon" /><h1> {evaluation.projectM}</h1>
+
+                                <p>¿Cómo de capaz eres de conceptualizar de manera funcional un proyecto digital?</p>
+                                <img id="WateringCan" src={WateringCan} alt="Evaluation result icon" /><h1> {evaluation.funcTech1}</h1>
+
+                                <p>¿Cómo de capaz eres de diseñar los procesos lógicos necesarios para un proyecto digital?</p>
+                                <img id="WateringCan" src={WateringCan} alt="Evaluation result icon" /><h1> {evaluation.funcTech2}</h1> 
+
+                                <p>¿Y de explicar la solución adoptada y presentar el trabajo realziado sintetizando sus planteamientos y resultados?</p>
+                                <img id="WateringCan" src={WateringCan} alt="Evaluation result icon" /><h1> {evaluation.funcTech3}</h1> 
+
+                                <p>¿Cómo de capaz eres de crear una UI web estática y adaptable?</p>
+                                <img id="WateringCan" src={WateringCan} alt="Evaluation result icon" /><h1> {evaluation.front1}</h1> 
+
+                                <p>¿Y de crear una UI web dinámica y adaptable?</p>                            
+                                <img id="WateringCan" src={WateringCan} alt="Evaluation result icon" /><h1> {evaluation.front2}</h1> 
+
+                                <p>¿Qué capaz eres de gestionar las estructuras, el flujo y el almacenamiento de datos de una web?</p>
+                                <img id="WateringCan" src={WateringCan} alt="Evaluation result icon" /><h1> {evaluation.back1}</h1> 
+
+                                <p>¿Y de de desarrollar algoritmos que ejecutan procesos lógicos?</p>
+                                <img id="WateringCan" src={WateringCan} alt="Evaluation result icon" /><h1> {evaluation.back2}</h1> 
+
+                                <p>¿Cómo de capaz eres de implementar arquitecturas web escalables?</p>
+                                <img id="WateringCan" src={WateringCan} alt="Evaluation result icon" /><h1> {evaluation.archit}</h1> 
+
+                                <p>¿Qué capaz eres de implementar prácticas y tecnologías para la gestión de versiones?</p>
+                                <img id="WateringCan" src={WateringCan} alt="Evaluation result icon" /><h1> {evaluation.qa1}</h1>
+
+                                <p>¿Cómo de capaz eres de implementar un código limpio y escalable?</p>
+                                <img id="WateringCan" src={WateringCan} alt="Evaluation result icon" /><h1> {evaluation.qa2}</h1> 
+
+                                <p>¿Y de implementar prácticas de QA para el aseguramiento y control de la calidad de una web?</p>
+                                <img  id="WateringCan" src={WateringCan} alt="Evaluation result icon" /><h1> {evaluation.qa3}</h1> 
+                            </div>
+                            </>
+                        )})}
+                    
+            </div>
+            <Footer/>
             </>
         )
     }
